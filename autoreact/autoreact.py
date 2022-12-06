51c8a4c35c214b5fec535faeb59ddc2358873b06
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-12-06 18:14:33 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -21,7 +21,7 @@ class Autoreact(commands.Cog):
             return
         if "ohio" in message.content.lower():
             await message.add_reaction("<a:AXHA:1006778596695744593>")
-        if int(667378631336525824) in message():
+        if 667378631336525824 == message.content():
             await message.add_reaction("<:emoji_21:1033650320636641310>") 
             await message.add_reaction("<:emoji_22:1033650337053167646>")      
         if 735018264651890689 in message.content():
