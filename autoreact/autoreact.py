2d481c68c0a7067043cdca765770b4d4cdfda76e
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-12-07 13:18:25 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -24,28 +24,28 @@ class Autoreact(commands.Cog):
             await message.add_reaction("<AXHA:1006778596695744593>")
         if "667378631336525824" in message.content:
             time.sleep(0.5) 
-            await message.add_reaction("<:emoji_22:1033650337053167646>" "<:emoji_21:1033650320636641310>") 
+            await message.add_reaction("<emoji_22:1033650337053167646>" "<emoji_21:1033650320636641310>") 
         if "735018264651890689" in message.content:
             time.sleep(0.5)
-            await message.add_reaction("<:emoji_35:1049663858874261545>")
+            await message.add_reaction("<emoji_35:1049663858874261545>")
         if "752152316596977704" in message.content:
             time.sleep(0.5)
-            await message.add_reaction("<:bc_o_carefree:984809639495479316>")
+            await message.add_reaction("<bc_o_carefree:984809639495479316>")
         if "chad" in message.content.lower():
             time.sleep(0.5)
-            await message.add_reaction("<:bc_z_chadmove:976826985546395678>")
+            await message.add_reaction("<bc_z_chadmove:976826985546395678>")
         if "851771484635398175" in message.content:
             time.sleep(0.5)
-            await message.add_reaction("<:bc_o_worry_foff:979426649470799942>")
+            await message.add_reaction("<bc_o_worry_foff:979426649470799942>")
         if "860808338442158130" in message.content:
             time.sleep(0.5)
-            await message.add_reaction("<:bc_z_CosmicChad:1048907381121224815>") 
+            await message.add_reaction("<bc_z_CosmicChad:1048907381121224815>") 
         if "hello" in message.content.lower():
             time.sleep(0.5)
             await message.add_reaction("😆")
         if "813107139601104917" in message.content: 
            time.sleep(0.5)
-           await message.add_reaction("<:bc_z_bhaiTuApna:1040891783976075314>")
+           await message.add_reaction("<bc_z_bhaiTuApna:1040891783976075314>")
 
 
 async def setup(bot):
