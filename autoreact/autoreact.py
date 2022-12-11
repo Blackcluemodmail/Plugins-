b40d0efdb4bc2efbbb117a5f816e7ced9b6db6c6
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-12-11 22:28:08 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -101,7 +101,7 @@ class Autoreact(commands.Cog):
         verified = message.guild.get_role(906615384969474048)
         
         # sticker permissions 
-        if lv10 or booster or srvstaff or chatmem or voicemem or trial or admin or ytmem or minecraftstaff or minecraftdev or verified or rankholder in message.author.roles:
+        if lv10 or booster or srvstaff or chatmem or voicemem or trial or admin or ytmem or minecraftstaff or minecraftdev or verified or rankholder in message.author.roles and message.stickers:
             return
         else:
             await message.delete()
