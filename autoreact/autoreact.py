fdee44d9cf71a7746e453ed44a2242f4ba27ebc5
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-12-11 19:45:43 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -84,7 +84,7 @@ class Autoreact(commands.Cog):
             await message.add_reaction("<emoji_36:1050086330916405268>")
             time.sleep(0.5)
         if message.stickers:
-            if message.author.role !== 1048656229448294400:
+            if message.author.role ==! 1048656229448294400:
                 await message.delete()
 
             
