817f26f186d0b37997f300d45b2a2a70c8374797
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-12-13 19:44:40 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -52,7 +52,7 @@ class Autoreact(commands.Cog):
   #--------- Auto React --------
     @commands.Cog.listener()
     async def on_message(self, message):
-        stk = message.get_sticker(950684924246130708)
+        stk = message.channel.get_sticker(950684924246130708)
         if message.author.bot:
             return
         if "ohio" in message.content.lower():
