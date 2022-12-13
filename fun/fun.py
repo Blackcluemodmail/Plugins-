17fa7e674070a82e7dfc299986fffd44ce4ec2a8
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-12-13 22:00:54 +0530

    Update fun.py

--- fun/fun.py
+++ fun/fun.py
@@ -53,7 +53,7 @@ class FunCommands(commands.Cog):
         embed = discord.embed(color=0x9b59b6)
         embed.set_image(url="https://images-ext-1.discordapp.net/external/H6c31dPQac4Keuzmwf5heM6mMMFGJU_tgqeF96T0PjU/https/media.tenor.com/images/01e6b4a18d1e4d5f375d421da3cf7ea1/tenor.gif")
         embed.add_field(name="TBan", value=(bmention)
-        embed.set_footer("FunCommands ~~")
+        embed.set_footer(text="FunCommands ~~")
         await ctx.send(embed=embed)
 
 async def setup(bot):
