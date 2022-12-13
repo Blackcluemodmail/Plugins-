aa11e7c86dbd569ced74412e8af2131c75dfd8bd
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-12-13 22:10:09 +0530

    Update fun.py

--- fun/fun.py
+++ fun/fun.py
@@ -49,10 +49,16 @@ class FunCommands(commands.Cog):
     @checks.has_permissions(PermissionLevel.MODERATOR)
     @commands.command()
     async def tban(self, ctx, member:discord.Member = None):
+        """ 
+        Bans a member from discord server!
+        (fun command)
+        """
+        if member == None:
+            await ctx.send_help(ctx.command)
         bmention = f"{member.mention} has been banned from **{member.guild}**"
         embed = discord.Embed(color=0x9b59b6)
         embed.set_image(url="https://images-ext-1.discordapp.net/external/H6c31dPQac4Keuzmwf5heM6mMMFGJU_tgqeF96T0PjU/https/media.tenor.com/images/01e6b4a18d1e4d5f375d421da3cf7ea1/tenor.gif")
-        embed.add_field(name="TBan", value=(bmention))
+        embed.add_field(name="", value=(bmention))
         embed.set_footer(text = "Fun Commands~~")
         await ctx.send(embed=embed)
 
