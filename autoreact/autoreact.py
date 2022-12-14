9ea003771756f21dfbce75bdcc811c52850f37e1
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-12-14 21:36:42 +0530

    Update autoreact.py

--- autoreact/autoreact.py
+++ autoreact/autoreact.py
@@ -167,8 +167,8 @@ class Autoreact(commands.Cog):
             time.sleep(30)
             await member.remove_roles(purchase)
                   
-        await ctx.channel.send(embed=embed)
-                              
+
+                             
                               
 
             
