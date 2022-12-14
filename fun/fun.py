f901670559535b9e4902b0bc50d6014631b94d64
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-12-14 23:18:13 +0530

    Update fun.py

--- fun/fun.py
+++ fun/fun.py
@@ -180,7 +180,7 @@ class FunCommands(commands.Cog):
         gc = message.guild.get_channel(892689877517365268)
         if message.channel == gc and admin not in message.author.roles and manager not in message.author.roles:
             if "!!help" in message.content or "<@892768539612696597> help" in message.content:
-                await message.author.delete()
+                await message.delete()
           
                 
                 
